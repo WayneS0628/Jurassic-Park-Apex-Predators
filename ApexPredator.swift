96cc00d6476cbf9bd273c3a21b397a7bc97ab970
wayneS0628 <wdsimmons2@icloud.com>
2024-07-01 23:44:09 -0400

    Add JP ApexPredators list in content view
    This commit introduced several new types of views, functions, and modifiers. Those
    new things were:
    
    - List
    - String functions
    - Preferred color scheme
    - Shadow
    - Clip shape
    
    Using the things learned above the following changes were made:
    
    - Added List view into content view which essentially create a for loop going through every ApexPredator
    decoded from the JSON file using our Predator functions
    - Added image string var in struct that took predator name and removed whitespace and made it lower
    to make it match the image name in assets file so we could incldue image in list view
    - Under list view added '.preferredColorScheme(.dark)' since the app looks better dark
    - Added a white shadow to images
    - Added a PredatorType enum that use string rawValues and whos cases are air, land, and sea
    to have an easier more streamlined way to  include the type for predators
    - Made the struct apexPredators' 'type' data member change its type from 'String' to our 'PredatorType' enum since the enum has
    string raw values which allowed us to include the type (land air or sea) more easily when we decode our apexPredators and show them
    - Used clipshape to make the type of the dinosaurs in the view appar in a capsule shape
    
    Other Changes:
    
    - Made ApexPredators struct conform to identifiable by adding identifiable next to struct name
    and ensuring we have an id property which we decoded
    - Added 'decodable' to the PredatorType enum so the error would dissappear
    - In content view where the text for predatorType is in the list, added '.rawValue' since we need to display strings
    and the 'type' data members is technically a custom enum now which thankfully we've made to have rawValues of strings

--- ApexPredator.swift
+++ ApexPredator.swift
@@ -6,19 +6,24 @@
 //
 
 import Foundation // Import the Foundation framework, which provides essential data types and utilities
+import SwiftUI
 
 // Define a struct called 'ApexPredator' that conforms to the 'Decodable' protocol
 // This struct represents the data structure for an apex predator in the JSON
 // 'Decodable' allows the struct to be initialized from JSON data
-struct ApexPredator: Decodable {
+struct ApexPredator: Decodable, Identifiable {
     let id: Int // Property to store the ID of the apex predator
     let name: String // Property to store the name of the apex predator
-    let type: String // Property to store the type (e.g., land, sea) of the apex predator
+    let type: PredatorType // Property to store the type (e.g., land, sea) of the apex predator
     let latitude: Double // Property to store the latitude coordinate of the apex predator's location
     let longitude: Double // Property to store the longitude coordinate of the apex predator's location
     let movies: [String] // Property to store a list of movies featuring the apex predator
     let movieScenes: [MovieScene] // Property to store a list of movie scenes featuring the apex predator
     let link: String // Property to store a URL link with more information about the apex predator
+    
+    var image: String {
+        name.lowercased().replacingOccurrences(of: " ", with: "")
+    }
     
     // Define a nested struct called 'MovieScene' that also conforms to the 'Decodable' protocol
     // This struct represents the data structure for a movie scene in the JSON
@@ -27,5 +32,22 @@ struct ApexPredator: Decodable {
         let id: Int // Property to store the ID of the movie scene
         let movie: String // Property to store the name of the movie
         let sceneDescription: String // Property to store the description of the scene
+    }
+    
+    enum PredatorType: String, Decodable{
+        case land
+        case air
+        case sea
+        
+        var background: Color {
+            switch self {
+            case .land:
+                    .brown
+            case .air:
+                    .teal
+            case .sea:
+                    .blue
+            }
+        }
     }
 }
